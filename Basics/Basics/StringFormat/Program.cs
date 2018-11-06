d0e0821843cb8d63c866a68a7c11cbd6762f6f4d
Unknown <folke.b@web.de>
2018-11-06 18:50:54 +0100

    Add Basic Sample solution

---
+++ Basics/Basics/StringFormat/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Logging.Serilog;
+using StringFormat.ViewModels;
+using StringFormat.Views;
+
+namespace StringFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BuildAvaloniaApp().Start<MainWindow>(() => new MainWindowViewModel());
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .UseReactiveUI()
+                .LogToDebug();
+    }
+}
---
+++ Basics/Basics/StringFormat/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace StringFormat.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private string _myText;
+        public string MyText
+        {
+            get { return _myText; }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _myText, value);
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            MyText = "world";
+        }
+    }
+}
