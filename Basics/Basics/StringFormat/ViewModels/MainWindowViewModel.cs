2c373d5dd3820f5e2b0cd26228d8192b740db7c3
Unknown <folke.b@web.de>
2018-11-06 19:00:16 +0100

    Update StringFormat

--- Basics/Basics/StringFormat/ViewModels/MainWindowViewModel.cs
+++ Basics/Basics/StringFormat/ViewModels/MainWindowViewModel.cs
@@ -17,9 +17,20 @@ namespace StringFormat.ViewModels
             }
         }
 
+        private float _value;
+        public float Value
+        {
+            get { return _value; }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _value, value);
+            }
+        }
+
         public MainWindowViewModel()
         {
             MyText = "world";
+            Value = 0.132f;
         }
     }
 }
